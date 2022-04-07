3a28d250e75d30f9a3143b31de415f5c8e9d4f7b
Fabian Silva <silvafabian74@yahoo.com>
2022-04-07 11:28:04 -0400

    fix indentation

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -7,8 +7,8 @@ core = Blueprint('core', __name__)
 @core.route('/')
 def index():
     
-    return render_template('index.html')
+  return render_template('index.html')
 
 @core.route('/info')
 def info():
-    return render_template('info.html')
+  return render_template('info.html')
--- myapp/models.py
+++ myapp/models.py
@@ -12,22 +12,22 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
+	__tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(64), unique=True, index=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(64), unique=True, index=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	password_hash = db.Column(db.String(128))
 
-    def __init__(self, email, username, password):
-        self.email = email
-        self.username = username
-        self.password_hash = generate_password_hash(password)
+	def __init__(self, email, username, password):
+		self.email = email
+		self.username = username
+		self.password_hash = generate_password_hash(password)
 
 #going to use this in our login view 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-    
-    def __repr__(self):
-        return f"Username {self.username}"
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
+	
+	def __repr__(self):
+		return f"Username {self.username}"
 
